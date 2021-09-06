a2bf91d0ceb78d50ac5f1409af2ec534fa2ef390
Jocelynchta06 <89814223+Jocelynchta06@users.noreply.github.com>
2021-09-06 17:50:40 -0500

    Update index.css

--- index.css
+++ index.css
@@ -1,1 +1,7 @@
 # practica1_Jocelyn
+fuction get IMC() {
+  var peso=parsefloat(document.getElementById('peso').value);
+   var altura=parsefloat(document.getElementById('altura').value);
+  get ElementById
+  var imc=peso/(altura * altura);
+  document.getElementById("imc").innerHTML
